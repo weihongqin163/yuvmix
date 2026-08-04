df4c9ce99a09705da3f7231d4dd4ef2605488f47
weihongqin <weihongqin@agora.io>
2026-08-04 12:11:24 +0800

    test: verify C API declarations

--- tests/integration/mix_yuv_c_api_test.c
+++ tests/integration/mix_yuv_c_api_test.c
@@ -2,6 +2,25 @@
 
 #include "video/mix_yuv_c.h"
 
+typedef yuvmix_status (*yuvmix_context_create_fn)(const yuvmix_config*,
+                                                   yuvmix_context**);
+typedef void (*yuvmix_context_destroy_fn)(yuvmix_context*);
+typedef yuvmix_status (*yuvmix_mix_fn)(yuvmix_context*,
+                                       const yuvmix_source*,
+                                       size_t,
+                                       yuvmix_output*);
+
+_Static_assert(_Generic(&yuvmix_context_create,
+                        yuvmix_context_create_fn: 1,
+                        default: 0),
+               "yuvmix_context_create signature changed");
+_Static_assert(_Generic(&yuvmix_context_destroy,
+                        yuvmix_context_destroy_fn: 1,
+                        default: 0),
+               "yuvmix_context_destroy signature changed");
+_Static_assert(_Generic(&yuvmix_mix, yuvmix_mix_fn: 1, default: 0),
+               "yuvmix_mix signature changed");
+
 int main(void) {
     yuvmix_config config = {0};
     yuvmix_source source = {0};
